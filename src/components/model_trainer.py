9558a73937c4221e867cc4982d766613b3d0d860
Mainak Dan <mainak.dan@grenoble-inp.fr>
2026-04-10 19:22:27 +0200

    updated model trainer with xgboost and arima

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -60,6 +60,14 @@ class ModelTrainer:
 
     def _train_prophet(self, model_class, param_grid):
         logging.info('Training and Tuning Prophet...')
+
+        dataset_freq = pd.infer_freq(self.train_df.index)
+
+        if dataset_freq is None:
+            logging.warning("Could not infer frequency from index. Defaulting to 'h'.")
+            dataset_freq = 'h'
+        else:
+            logging.info(f"Dynamically inferred dataset frequency: {dataset_freq}")
         
         train_df_prophet = self.train_df.reset_index().rename(
             columns={'Datetime': 'ds', self.target: 'y'})
@@ -75,7 +83,7 @@ class ModelTrainer:
             model = model_class(**params)
             model.fit(train_df_prophet)
 
-            future = model.make_future_dataframe(periods=len(self.test_df), freq='h')
+            future = model.make_future_dataframe(periods=len(self.test_df), freq=dataset_freq)
             forecast = model.predict(future)
             forecast_prophet = forecast.set_index('ds')['yhat'][-len(self.test_df):]
             metrics = self.evaluate_metrics("Prophet", self.test_df[self.target], 
@@ -85,13 +93,63 @@ class ModelTrainer:
                 best_r2 = metrics["R2"]
                 best_model = model
                 best_metrics = metrics
-                
+        logging.info('Prophet training and tuning completed!')
         return best_model, best_metrics
 
-    def _train_xgboost(self, train, test):
-        pass
-    def _train_arima(self, train, test):
-        pass
+    def _train_xgboost(self, base_model, param_grid):
+        logging.info('Training and tuning XGBoost...')
+
+        def create_xgb_features(df):
+            df_xgb = df.copy()
+            df_xgb['hour'] = df_xgb.index.hour
+            df_xgb['day'] = df_xgb.index.day
+            df_xgb['weekday'] = df_xgb.index.weekday
+            df_xgb['month'] = df_xgb.index.month
+
+            for i in range(24):
+                df_xgb[f'lag_{i+1}'] = df_xgb[self.target].shift(i+1)
+            df_xgb.dropna(inplace=True)
+            return df_xgb
+
+        full_data = pd.concat([self.train_df, self.test_df])
+        xgb_data = create_xgb_features(full_data)
+
+        train_len = len(self.train_df) - 24
+        X = xgb_data.drop(self.target, axis=1)
+        y = xgb_data[self.target]
+
+        X_train, y_train = X.iloc[:train_len], y.iloc[:train_len]
+        X_test, y_test = X.iloc[train_len:], y.iloc[train_len:]
+
+        grid_search = GridSearchCV(estimator=base_model, param_grid=param_grid, cv=3, scoring='r2', n_jobs=-1)
+        grid_search.fit(X_train, y_train)
+
+        best_model = grid_search.best_estimator_
+        y_pred = best_model.predict(X_test)
+        
+        metrics = self.evaluate_metrics("XGBoost", y_test, y_pred)
+        logging.info('XGBoost training and tuning completed!')
+
+        return best_model, metrics
+
+
+    def _train_arima(self, model_func, param_grid):
+        logging.info('Training Auto-ARIMA')
+        y_train = self.train_df[self.target]
+        y_test = self.test_df[self.target]
+
+        model = model_func(y_train, start_p=0, start_q=0, 
+                           max_p=5, max_q=5, m=1, 
+                           trace=False, error_action='ignore', 
+                           suppress_warnings=True, 
+                           stepwise=True)
+        
+        forecast = model.predict(n_periods=len(y_test))
+        metrics = self.evaluate_metrics("ARIMA", y_test, forecast)
+
+        logging.info('Training of Auto-ARIMA completed!')
+
+        return model, metrics
 
     def initiate_model_trainer(self, train_path, test_path):
         try:
